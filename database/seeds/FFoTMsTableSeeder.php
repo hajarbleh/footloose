73f6a95861cd7d5ab4d4061dc4c318df5d1a7cbb
hajarbleh <johnstephanus@ymail.com>
2017-08-15 15:48:42 +0700

    Add seeder for FFoTM table

--- database/seeds/FFoTMsTableSeeder.php
+++ database/seeds/FFoTMsTableSeeder.php
@@ -17,5 +17,15 @@ class FFoTMsTableSeeder extends Seeder
         $FFoTM1->strap_id = 1;
         $FFoTM1->category_id = 2;
         $FFoTM1->save();
+        $FFoTM2 = new FFoTM;
+        $FFoTM2->base_id = 1;
+        $FFoTM2->strap_id = 1;
+        $FFoTM2->category_id = 2;
+        $FFoTM2->save();
+        $FFoTM3 = new FFoTM;
+        $FFoTM3->base_id = 1;
+        $FFoTM3->strap_id = 1;
+        $FFoTM3->category_id = 2;
+        $FFoTM3->save();
     }
 }
