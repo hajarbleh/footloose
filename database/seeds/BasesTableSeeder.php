c34cb3d46e6dfc5f863377a0b5e9f1ab06c0e3d5
CYNDE\cynde_ <cynthiacyndewi@gmail.com>
2017-09-04 23:02:29 +0700

    seeder for base & strap added

--- database/seeds/BasesTableSeeder.php
+++ database/seeds/BasesTableSeeder.php
@@ -11,6 +11,22 @@ class BasesTableSeeder extends Seeder
      */
     public function run()
     {
-
+        $base1 = new Base;
+        $base1->name="pink";
+        $base1->color="#FFFFFF";
+        $base1->size=37;
+        $base1->category_id=1;
+        $base1->stock=100;
+        $base1->picture="";
+        $base1->save();
+        
+        $base2 = new Base;
+        $base2->name="black";
+        $base2->color="#FFFFFF";
+        $base2->size=37;
+        $base2->category_id=1;
+        $base2->stock=100;
+        $base2->picture="";
+        $base2->save();
     }
 }
--- database/seeds/StrapsTableSeeder.php
+++ database/seeds/StrapsTableSeeder.php
@@ -11,6 +11,22 @@ class StrapsTableSeeder extends Seeder
      */
     public function run()
     {
-
+        $strap1 = new Strap;
+        $strap1->name="pink";
+        $strap1->color="#FFFFFF";
+        $strap1->size=37;
+        $strap1->category_id=1;
+        $strap1->stock=100;
+        $strap1->picture="";
+        $strap1->save();
+        
+        $strap2 = new Strap;
+        $strap2->name="black";
+        $strap2->color="#FFFFFF";
+        $strap2->size=37;
+        $strap2->category_id=1;
+        $strap2->stock=100;
+        $strap2->picture="";
+        $strap2->save();
     }
 }
